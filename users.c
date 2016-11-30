e9b84728f88580d2c4211eb127d587fb3061f92e
Andrew Pamment <apamment@yandex.com>
2016-11-30 10:38:09 +1000

    Allow Spaces in usernames

--- users.c
+++ users.c
@@ -431,7 +431,7 @@ struct user_record *new_user() {
 			}
 
 			for (i=0;i<strlen(buffer);i++) {
-				if (!(tolower(buffer[i]) >= 97 && tolower(buffer[i]) <= 122)) {
+				if (!(tolower(buffer[i]) >= 97 && tolower(buffer[i]) <= 122) && buffer[i] != 32) {
 					s_printf(get_string(168));
 					nameok = 1;
 					break;
