16403f14ad985a9bf2c8378970194b58c37d7978
Andrew Pamment <apamment@yandex.com>
2018-02-09 22:25:25 +1000

    Prefer Jenkins. Sorry Travis

--- .travis_install_deps.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-wget https://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.59.tar.gz
-tar xzf libmicrohttpd-0.9.59.tar.gz
-cd libmicrohttpd-0.9.59
-./configure
-make
-sudo make install
